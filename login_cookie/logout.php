0c0671f93b94a68b9912223e50478725eabc0d18
Dishita28 <143996968+Dishita28@users.noreply.github.com>
2024-11-17 21:38:06 +0530

    Delete login_cookie directory

--- login_cookie/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-setcookie("username",time()-3600);
-header("Location: language.php");
-?>
--- login_cookie/welcome.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username=$_COOKIE['username'];
-echo "your username=". $username."<br>";
-?>
-<html>
-<body>
-	<a href="logout.php"> logout </a> 
-</body>
-</html>
